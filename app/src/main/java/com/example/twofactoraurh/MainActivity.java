90885fa592f37d3264c1ebbb46cc6b5ca6cd8a3f
clementleroux16 <cleroux1601@gmail.com>
2025-01-29 10:44:25 +0100

    Version 0.1.0

--- app/src/main/java/com/example/twofactoraurh/MainActivity.java
+++ app/src/main/java/com/example/twofactoraurh/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.twofactoraurh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import android.view.View.OnClickListener;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+    TextView code_genere;
+    EditText username;
+    Button generer;
+    String code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        code_genere = findViewById(R.id.code_genere);
+        username = findViewById(R.id.username);
+        generer = findViewById(R.id.generer);
+
+        generer.setOnClickListener(this);
+
     }
+
+    private String generateCode(String username) {
+        long timestamp = System.currentTimeMillis() / 1000 / 30;
+        return String.valueOf((username.hashCode() + timestamp) % 1000000);
+    }
+
+    public void onClick(View v){
+        code = generateCode(username.toString());
+        Intent intent = new Intent(MainActivity.this, activity_auth.class);
+        intent.putExtra("username",username.getText().toString());
+        intent.putExtra("code",code);
+
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/twofactoraurh/activity_auth.java
+++ app/src/main/java/com/example/twofactoraurh/activity_auth.java
@@ -1,6 +1,10 @@
 package com.example.twofactoraurh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class activity_auth extends AppCompatActivity {
-
+    Button verif_code;
+    EditText entree_code;
+    TextView tv_verif;
+    String username, code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_auth);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        verif_code = (Button) findViewById(R.id.verif_code);
+        entree_code = (EditText) findViewById(R.id.code);
+        tv_verif = (TextView) findViewById(R.id.tv_verif);
+
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+        code = intent.getStringExtra("code");
+
+        tv_verif.setText(username);
     }
 }
